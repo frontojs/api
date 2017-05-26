a0baac1f0a24ac63237f546b5940d49be4e269e5
Zack Siri <zack@codemy.net>
2017-05-26 17:55:50 +0700

    use args object instead

--- src/index.js
+++ src/index.js
@@ -1,38 +1,38 @@
-export default (endpoint, options = { header: null, request: {}}) => {
+export default (args = {endpoint: '', header: null, request: {}}) => {
   const headers = () => {
     const h = new Headers();
 
     h.append('Content-Type', 'application/json');
-    if (options.header) options.header(h);
+    if (args.header) args.header(h);
     return h;
   };
 
   const request = (endpoint, method, resource, body) => {
     const url = [endpoint, resource].join("");
-    const reqOptions = { method, headers: headers(), ...options.request };
+    const options = { method, headers: headers(), ...args.request };
 
     if (body) {
       options.body = JSON.stringify(body);
     }
 
-    return fetch(new Request(url, reqOptions));
+    return fetch(new Request(url, options));
   };
 
   return {
     get(_path) {
-      return request(endpoint, 'GET', _path);
+      return request(args.endpoint, 'GET', _path);
     },
 
     post(_path, data = {}) {
-      return request(endpoint, 'POST', _path, data);
+      return request(args.endpoint, 'POST', _path, data);
     },
 
     patch(_path, data = {}) {
-      return request(endpoint, 'PATCH', _path, data);
+      return request(args.endpoint, 'PATCH', _path, data);
     },
 
     delete(_path) {
-      return request(endpoint, 'DELETE', _path);
+      return request(args.endpoint, 'DELETE', _path);
     },
   }
 };
